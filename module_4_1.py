406d94b524da52d0be2c230d493156333020f37e
BoldyrevaMarya <matazzz@mail.ru>
2024-07-25 18:32:29 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,10 @@
+from fake_math import divide as fd
+from true_math import divide as td
+result1 = fd(55, 3)
+result2 = fd(8, 0)
+result3 = td(74, 4)
+result4 = td(32, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
